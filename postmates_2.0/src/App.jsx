64f64f17365f2eeb34d5a7fb55a7db6010d97586
G-Andrey <g.andrey77@gmail.com>
2020-10-12 16:06:39 -0400

    ignore .vscode

--- postmates_2.0/src/App.jsx
+++ postmates_2.0/src/App.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
+import {
+  Nav,
+  Navbar,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+} from 'react-bootstrap';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -16,6 +23,20 @@ export default class App extends React.Component{
   render() {
     return (
       <>
+      <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+        <Navbar.Brand>WatchParty</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Form inline onSubmit={(event) => { event.preventDefault(); }}>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-info">Search</Button>
+            </Form>
+          </Nav>
+          <Nav>
+          </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </>
     )
   }
