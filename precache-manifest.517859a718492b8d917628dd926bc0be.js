b3ed8e3f1adb08cbcf14817506f50695e39e8b3b
Syed Sadman <syedsadman16@gmail.com>
2020-11-01 11:42:08 -0500

    Updates

---
+++ precache-manifest.517859a718492b8d917628dd926bc0be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb2b486d3a88d2da2e2426dfcef0abd3",
+    "url": "/Online-Restaurant-System-Frontend/index.html"
+  },
+  {
+    "revision": "c4dc1bbe6f200f5eb0af",
+    "url": "/Online-Restaurant-System-Frontend/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "36fcd4cb2f5d758f8df4",
+    "url": "/Online-Restaurant-System-Frontend/static/css/main.9a62dc5e.chunk.css"
+  },
+  {
+    "revision": "c4dc1bbe6f200f5eb0af",
+    "url": "/Online-Restaurant-System-Frontend/static/js/2.fbd3c21b.chunk.js"
+  },
+  {
+    "revision": "34e53bacd4ca8e0cec3c2da4a1f93fb2",
+    "url": "/Online-Restaurant-System-Frontend/static/js/2.fbd3c21b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36fcd4cb2f5d758f8df4",
+    "url": "/Online-Restaurant-System-Frontend/static/js/main.b17e8a3e.chunk.js"
+  },
+  {
+    "revision": "8a6e66e981e531299ce0",
+    "url": "/Online-Restaurant-System-Frontend/static/js/runtime-main.7a1156cd.js"
+  }
+]);
