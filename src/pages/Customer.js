b8332c7d6c35a1f669bcbb1645318706415302d4
G-Andrey <42978646+G-Andrey@users.noreply.github.com>
2020-11-30 16:44:32 -0500

    undid top 3 orders

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -1,12 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import api from "axios";
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCookie } from "@fortawesome/free-solid-svg-icons";
 
 const Customer = () => {
   const user = useSelector(({ user }) => user)
@@ -16,59 +10,11 @@ const Customer = () => {
       {user.role === "VIP" ? <>***VIP***</> : <>***Regular Customer***</>}
     </div>
   );
-  const [allCustomerOrders, setAllCustomerOrders ] = useState([]);
-  
-  const fetchAllCustomerOrders = async() => {
-    try{
-      let {
-        data: orders
-      } = await api.get(`/orders`);
-      setAllCustomerOrders(orders);
-    }
-    catch(E){console.log(E)};
-  };
-
-  useEffect( () =>{
-    fetchAllCustomerOrders();
-  }, []);
 
   return (
     <div>
-      {console.log(allCustomerOrders)}
       {showStatus}
-      <h2>
-        Top 3 Ordered Dishes:
-      </h2>
-      {
-        allCustomerOrders.length == 0 ?
-          <div>
-            You have no orders so far
-          </div>
-        :
-        allCustomerOrders.length > 0 ?
-          <List>
-            <ListItem key={allCustomerOrders[0].id} dense button>
-              <ListItemIcon >
-                <FontAwesomeIcon icon={faCookie} size="1x" color="gray" />
-              </ListItemIcon>
-              <ListItemText primary={allCustomerOrders[0].dishOrders[0].dish.name} />
-            </ListItem>
-            <ListItem key={allCustomerOrders[1].id} dense button>
-              <ListItemIcon >
-                <FontAwesomeIcon icon={faCookie} size="1x" color="gray" />
-              </ListItemIcon>
-              <ListItemText primary={allCustomerOrders[1].dishOrders[0].dish.name} />
-            </ListItem>
-            <ListItem key={allCustomerOrders[2].id} dense button>
-              <ListItemIcon >
-                <FontAwesomeIcon icon={faCookie} size="1x" color="gray" />
-              </ListItemIcon>
-              <ListItemText primary={allCustomerOrders[2].dishOrders[0].dish.name} />
-            </ListItem>
-          </List>
-        :
-        null
-      }
+      More customer stuff
     </div>
   );
 };
