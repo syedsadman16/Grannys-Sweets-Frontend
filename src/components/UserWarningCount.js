a0d7bfb843d1d2fe8eee0417b35adebb2947d236
michali123 <42022911+michali123@users.noreply.github.com>
2020-12-01 02:57:14 -0500

    Fixed UserWarningCount component

--- src/components/UserWarningCount.js
+++ src/components/UserWarningCount.js
@@ -1,43 +1,26 @@
 import React, { Component } from "react";
-import axios from 'axios'
-
-const api = axios.create({
-    warnningURL: 'http://localhost:3000/api/warning'
-})
+import axios from "axios";
 
 class Warning extends Component{
+  state = {warning:[]  };
 
-  state = {warning:[]
+  componentDidMount(){
+    axios
+    .get("/warning").then((response) => {
+      console.log(response.data);
+    })
+    .catch((e) => console.log(e));
+    }
+
+    render(){
+      return(
+        <div className="Warnings">
+          <header className="warning header">
+            <p>(this.state.warning.length)</p>
+          </header>
+        </div>
+      );
+    }
   }
 
-constructor() {
-    super();
-    api.get('/').then(res => {
-        console.log(res.data)
-        this.setState({warning: res.data})
-    })
-}
-
-render(){
-  return(
-    <div className= "Warnings">
-      <header className = "Warnings header">
-        {this.state.warning.map(warning => <h2 key = {warning.id}></h2>)}
-      </header>
-    </div>
-  )
-}
-}
-/*  
-const fetchData = async () => {
-    try {
-      let {
-        data: { balance },
-      } = await api.get(`/users/balance`);
-      setBalance(balance);
-
-      let { data: transactions } = await api.get(`/transactions`);
-      setTransactions(transactions);
-    } catch (e) {}
-  };
-*/
+  export default Warning;
