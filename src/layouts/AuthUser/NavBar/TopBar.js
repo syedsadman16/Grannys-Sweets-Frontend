415039bc55d06d57e853e6444811b74404a6c989
G-Andrey <42978646+G-Andrey@users.noreply.github.com>
2020-12-01 22:27:19 -0500

    rebuilding authuser navbar to look same as not rest

--- src/layouts/AuthUser/NavBar/TopBar.js
+++ src/layouts/AuthUser/NavBar/TopBar.js
@@ -14,7 +14,14 @@ import {
 import MenuIcon from "@material-ui/icons/Menu";
 import NotificationsIcon from "@material-ui/icons/NotificationsOutlined";
 import InputIcon from "@material-ui/icons/Input";
-//import Logo from 'src/components/Logo';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCartPlus,
+  faBriefcase,
+  faCookieBite,
+} from "@fortawesome/free-solid-svg-icons";
+
+import './TopBar.css'
 
 const useStyles = makeStyles(() => ({
   root: {},
@@ -29,9 +36,11 @@ const TopBar = ({ className, onMobileNavOpen, ...rest }) => {
   const [notifications] = useState([]);
 
   return (
-    <AppBar className={clsx(classes.root, className)} elevation={0} {...rest}>
+    <AppBar className={clsx(classes.root, className)} elevation={0}  {...rest}>
       <Toolbar>
-        <RouterLink to="/">Logo</RouterLink>
+        <RouterLink to="/">
+          <FontAwesomeIcon icon={faCookieBite} size="2x" color="white" />
+        </RouterLink>
         <Box flexGrow={1} />
         <Hidden mdDown>
           <IconButton color="inherit">
