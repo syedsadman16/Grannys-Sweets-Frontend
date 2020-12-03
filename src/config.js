3f540873b60dbcc56b090f919770d734168ce20e
smfils1 <smfils1@gmail.com>
2020-12-02 19:32:25 -0500

    Clean up and added backend url env

--- src/config.js
+++ src/config.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 import { setJwtHeader } from "./utils/jwt";
 
-const BACKEND_URL = `http://localhost:8080/api`;
+const BACKEND = process.env.REACT_APP_BACKEND_URL;
+
+const BACKEND_URL =
+  process.env.NODE_ENV === "production" ? BACKEND : `http://localhost:8080/api`;
 
 const configAxios = (token) => {
   axios.defaults.baseURL = BACKEND_URL;
