b1a0d08741f107a51443ed56f4825f7bafa1d2e4
rtx-abir <58044469+rtx-abir@users.noreply.github.com>
2020-12-05 13:50:40 -0500

    added form styling

--- src/pages/Employee/DishForm.js
+++ src/pages/Employee/DishForm.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import "./DishForm.css";
+import "./DishForm.scss";
 import { useSelector } from "react-redux";
 import { faUtensils } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -11,7 +11,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
     const [imageUrl, setImageUrl] = useState("");
     const [special,setSpecial] = useState(false);
     const [keyWord, setKeyWord] = useState([]);
-    const [allkeyWord, setAllKeyWord] = useState(["sweet","cake","Breakfast","Lunch","Dinner"]);
+    const [allkeyWord, setAllKeyWord] = useState(["Sweet","Cake","Breakfast","Lunch","Dinner"]);
     const [currWord, setcurrKeyWord] = useState("");
     const [price, setPrice] = useState("");
     const id = useSelector(({ user }) => user.id);
@@ -96,44 +96,44 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
     return(
   
-
+      <div class = "box">
         <form className="DishForm" onSubmit = {handleSubmission}>  
-            <h1 align = "center">
-            <FontAwesomeIcon icon={faUtensils} color="gray" />
-              Make your dish!!!
-            <FontAwesomeIcon icon={faUtensils} color="gray"/></h1>
+            <h1 class = "logo" align = "center">
+            </h1>
                  
             {console.log(keyWord)}
             {console.log(allkeyWord)}
             {console.log(currWord)}
-
-            <section align = "center">
-              <label htmlFor = "Name">Dish Name : </label>
-              <br></br>
-              <input name="Name" 
-              id = "Name" 
+            <br></br>
+            <div class = "input-container" >
+              <input name="Name"
+              id = "Name"
               value = {name}
               onChange = {handleNameChange}
               required/>
-            </section>
+              <label htmlFor = "Name">Dish Name</label>
+            </div>
             <br></br>
 
-            <section align = "center">
-              <label htmlFor = "Description">Description : </label>
-              <br></br>
-              <textarea 
-              name="Description" 
-              id = "Description" 
-              placeholder = "Enter your Dish's Description"
-              value = {description}
-              onChange = {handleDescriptionChange}
-              required/>
-            </section>
+            
+            <div class = "input-container" align = "center">
+              <section align = "center">
+                <input 
+                  name="Description" 
+                  id = "Description" 
+                  value = {description}
+                  onChange = {handleDescriptionChange}
+                  required/>
+                <label htmlFor = "Description"> Enter your Dish's Description</label>
+              </section>
+            </div>
             <br></br>
 
-            <section align = "center">
+            
+  
+
+            <section>
               <label htmlFor = "isSpecial">Special Type</label>
-              <br></br>              
               <select name="isSpecial" 
               id = "isSpecial"
               value = {special}
@@ -143,68 +143,67 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
                 <option value = {true}>Special</option>
               </select>
             </section>
+
+            <br></br>
+            <div class = "input-container" align = "center">
+              <section align = "center">
+
+                <input name="image" 
+                id = "image" 
+                value = {imageUrl}
+                onChange = {handleImageUrlChange}
+                required />
+                <label htmlFor = "image">Enter a valid image URL </label>
+              </section>
+            </div>
+            
             <br></br>
 
-            <section align = "center">
-              <label htmlFor = "image">Image URL : </label>
-              <br></br>
-              <input name="image" 
-              id = "image" 
-              placeholder = "Enter a valid image URL for your Dish"
-              value = {imageUrl}
-              onChange = {handleImageUrlChange}
-              required />
-            </section>
-            <br></br>
 
-            <section align = "center">
-              <label htmlFor = "price">Price</label>
-              <br></br>
-              <input type = "number" 
-              name="price" 
-              id = "price"
-              min = "0.00" 
-              step = "0.50"
-              placeholder = "0.00"
-              value = {price}
-              onChange = {handlePriceChange}
-              required/>
-            </section>
+            <div class = "input-container" align = "center">
+              <section align = "center">
+                <input type = "number" 
+                name="price" 
+                id = "price"
+                min = "0.00" 
+                step = "0.50"
+                value = {price}
+                onChange = {handlePriceChange}
+                required/>
+                <label htmlFor = "price">Price: </label>
+              </section>
+            </div>
+            
+
             <br></br>
             
           <section align = "center">
             <span>Give your Dish Some Keywords for people to find it :  (check at least one)</span>
             <br></br>
 
-            {/* <input type="checkbox" name = "sweet" id = "sweet" value = "sweet" onChange = {handleKeyword}/>
-            <label htmlFor = "sweet"> sweet </label>
-            <input type="checkbox" name = "cake" id = "cake" value = "cake" onChange = {handleKeyword}/>
-            <label htmlFor = "cake"> cake </label>
-            <input type="checkbox" name = "Breakfast" id = "BreakFast" value = "BreakFast" onChange = {handleKeyword}/>
-            <label htmlFor = "BreakFast"> BreakFast </label>
-            <input type="checkbox" name = "Lunch" id = "Lunch" value = "Lunch" onChange = {handleKeyword}/>
-            <label htmlFor = "Lunch"> Lunch </label>
-            <input type="checkbox" name = "Dinner" id = "Dinner" value = "Dinner" onChange = {handleKeyword}/>
-            <label htmlFor = "Dinner"> Dinner </label>
-            <br></br> */}
-
             {allkeyWord.map(word => 
               <>
-              <label htmlFor = {word}> {word} </label>
-              <input type="checkbox" name = {word} id = {word} value = {word} onChange = {handleKeyword}/>
+              <label align = "center" class="custom-checkbox" htmlFor = {word}> {word} 
+              <input type="checkbox"  name = {word} id = {word} value = {word} onChange = {handleKeyword}/>
+              <span class="checkmark"></span>
+              </label>
               </>
               )}
-          
+          </section>
 
-              <label htmlFor = "other">Other : </label>
               <br></br>
-              <input name="other" id = "other"  onChange = {handleCurrkeyword} value = {currWord} placeholder = "Other keywords" />
-              <br></br>
-              <button className="dishform-submit-btn"  onClick={handleAddKeyword}>
+              
+          <div class = "input-container" align = "center">
+            <section>
+              <input name="other" id = "other"  onChange = {handleCurrkeyword} value = {currWord} />\
+              <label htmlFor = "other">Other keywords: </label>
+            </section>
+          </div>
+
+          <section align = "center">
+              <button className="dishform-btn"  onClick={handleAddKeyword}>
                 Add Keywords
-            </button>
-            
-
+              </button>
           </section>
           <br></br>
 
@@ -213,6 +212,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
           </section>
             
         </form>
+      </div>
+        
   
         
     );
