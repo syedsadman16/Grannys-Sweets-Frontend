20eba89dab55ecaa2602f333e861651eb6589bbd
Syed Sadman <syedsadman16@gmail.com>
2020-12-08 18:11:04 -0500

    heroku connection

--- src/config.js
+++ src/config.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 import { setJwtHeader } from "./utils/jwt";
 
-const BACKEND = process.env.REACT_APP_BACKEND_URL;
 
-const BACKEND_URL =
-  process.env.NODE_ENV === "production" ? BACKEND : `http://localhost:8080/api`;
+const BACKEND_URL = `https://online-restaurant-system.herokuapp.com/api`;
 
 const configAxios = (token) => {
   axios.defaults.baseURL = BACKEND_URL;
